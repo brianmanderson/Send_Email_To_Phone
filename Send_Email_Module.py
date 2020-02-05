26fdfb529e621a214b971f1b9ba976499180ff2d
bmanderson@mdanderson.org <bmanderson@mdanderson.org>
2020-02-05 11:07:21 -0600

    first push

---
+++ Send_Email_Module.py
@@ -0,0 +1,38 @@
+__author__ = 'Brian M Anderson'
+# Created on 2/5/2020
+
+import smtplib, ssl
+
+
+class Send_Email(object):
+    def __init__(self, email_address, password, port=465):
+        # Create a secure SSL context
+        self.port = port
+        self.email_address = email_address
+        self.password = password
+        self.context = ssl.create_default_context()
+        self.login()
+
+    def set_outbound_email(self, email_address):
+        self.outbound_email = email_address
+
+    def login(self):
+        self.server = smtplib.SMTP_SSL("smtp.gmail.com", self.port, context=self.context)
+        self.server.login(self.email_address, self.password)
+
+    def send_email(self, message,outbound_email=None):
+        if outbound_email is None:
+            outbound_email = self.outbound_email
+        self.server.sendmail(self.email_address,outbound_email,message)
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
+
+    #email_class_object = Send_Email('email@gmail.com','password')
+    #email_class_object.set_outbound_email('phonenumber@vtext.com')
+    #email_class_object.send_email('Test')
